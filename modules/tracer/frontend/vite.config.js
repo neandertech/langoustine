ecc7e9a552953347024c00aeec285a4139ef3505
Anton Sviridov <keynmol@gmail.com>
2022-08-27 20:21:52 +0100

    Update docs

--- modules/tracer/frontend/vite.config.js
+++ modules/tracer/frontend/vite.config.js
@@ -5,8 +5,6 @@
 import path from 'path'
 
 export default () => {
-  // root: path.resolve(__dirname, './../target/js-3'),
-
   const port = process.env.LANGOUSTINE_PORT;
   const str = `http://localhost:${port}`;
   return {
