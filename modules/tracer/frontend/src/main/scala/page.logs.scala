47ee8178e558e9570b035a6a961be50124d09031
Anton Sviridov <keynmol@gmail.com>
2022-08-29 15:00:24 +0100

    cleanup

--- modules/tracer/frontend/src/main/scala/page.logs.scala
+++ modules/tracer/frontend/src/main/scala/page.logs.scala
@@ -5,7 +5,6 @@ import com.github.plokhotnyuk.jsoniter_scala.core.*
 import scala.scalajs.js.Date
 
 def logsTracer(logs: Var[Vector[String]], logFilter: Var[Option[String]]) =
-  import Message.*
   div(
     Styles.logTracer.container,
     input(
@@ -17,11 +16,12 @@ def logsTracer(logs: Var[Vector[String]], logFilter: Var[Option[String]]) =
     pre(
       code(
         children <--
-          logs.signal.combineWith(logFilter.signal).map { case (lines, f) =>
-            lines
-              .filter(l => f.isEmpty || f.exists(l.toLowerCase().contains))
-              .map(p(_))
-          }
+          logs.signal
+            .combineWithFn(logFilter.signal) { case (lines, f) =>
+              lines
+                .filter(l => f.isEmpty || f.exists(l.toLowerCase().contains))
+            }
+            .map(_.map(p(_)))
       )
     )
   )
