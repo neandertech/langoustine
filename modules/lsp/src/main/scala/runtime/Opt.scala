161dfc48e70fbddfe3ef3d3f49947ffacd49f079
Anton Sviridov <keynmol@gmail.com>
2026-04-15 09:50:53 +0100

    Remove Opt

--- modules/lsp/src/main/scala/runtime/Opt.scala
+++ modules/lsp/src/main/scala/runtime/Opt.scala
@@ -20,47 +20,29 @@ package runtime
 import scala.reflect.TypeTest
 
 import langoustine.*
-// import langoustine.lsp.json.*
-import upickle.default.*
 
-opaque type Opt[+A] = A | Null
-object Opt:
-  val empty: Opt[Nothing] = null
+@deprecated(
+  "Opt no longer exists in Langoustine - use Option instead. This alias is left for convenience and will be removed in 0.1.0",
+  "0.1.0"
+)
+type Opt[+A] = Option[A]
 
-  inline def apply[A](a: A): Opt[A] = a
+@deprecated(
+  "Opt no longer exists in Langoustine - use Option instead. This forwarder is left for convenience and will be removed in 0.1.0",
+  "0.1.0"
+)
+val Opt = Option
 
-  inline def fromOption[A](o: Option[A]): Opt[A] =
-    o.orNull.asInstanceOf[Opt[A]]
-
-  inline def unapply[A](o: Opt[A]): Option[A] =
-    if o != null then Some(o.asInstanceOf[o.type & A])
-    else None
-
-  extension [A](o: Opt[A])
-    inline def toOption = if o == empty then None else Some(o.asInstanceOf[A])
-
-  given [A]: TypeTest[Opt[A], A] with
-    inline def unapply(o: Opt[A]): Option[o.type & A] =
-      if o != null then Some(o.asInstanceOf[o.type & A])
-      else None
-
-  given [A]: CanEqual[Opt[A], Opt[A]] = CanEqual.canEqualAny
-  given [A]: CanEqual[Opt[A], Null]   = CanEqual.canEqualAny
-
-  // given [A](using
-  //     rd: Reader[A]
-  // ): Reader[Opt[A]] =
-  //   jsReader.map[Opt[A]] {
-  //     case ujson.Null => empty
-  //     case other      => upickle.default.read(other)(using rd)
-  //   }
-
-  // given [A](using
-  //     wt: Writer[A]
-  // ): Writer[Opt[A]] =
-  //   jsWriter.comap[Opt[A]] {
-  //     case other => upickle.default.writeJs(other.asInstanceOf[A])(using wt)
-  //     case null  => ujson.Null
-  //   }
-
-end Opt
+extension [A](o: Opt.type) 
+  
+  @deprecated(
+    "Opt no longer exists in Langoustine - use Option instead. This forwarder is left for convenience and will be removed in 0.1.0",
+    "0.1.0"
+  )
+  inline def fromOption(opt: Option[A]) = opt
+extension [A](o: Opt[A]) 
+  @deprecated(
+    "Opt no longer exists in Langoustine - use Option instead. This forwarder is left for convenience and will be removed in 0.1.0",
+    "0.1.0"
+  )
+  inline def toOption                     = o
