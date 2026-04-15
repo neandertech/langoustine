89f3642a82cb2f63ddd7e49e541c4e22165cc1ed
Anton Sviridov <keynmol@gmail.com>
2026-04-15 13:33:31 +0100

    Rework LSP tests

--- modules/tests/src/test/scala/LSPTests.scala
+++ modules/tests/src/test/scala/LSPTests.scala
@@ -16,93 +16,184 @@
 
 package tests.core
 
+import scala.concurrent.duration.*
+
+import cats.effect.IO
+import jsonrpclib.fs2.catsMonadic
+import jsonrpclib.{fs2 as _, *}
 import langoustine.lsp.*
-import jsonrpclib.Monadic
-import scala.util.*
-
 import langoustine.lsp.all.*
+import langoustine.lsp.structures.InitializeParams.ClientInfo
+import langoustine.lsp.structures.InitializeResult.ServerInfo
 
-// def basicServer[F[_]: Monadic] =
-//   LSPBuilder.create[F]
 
-// import jsonrpclib.*
 
-// object LSPTests extends weaver.FunSuite:
-//   test("initialize") {
+object LSPTests extends weaver.SimpleIOSuite:
 
-//     import requests.*
+  def serverTest(tn: weaver.TestName)(
+      f: weaver.Log[IO] => IO[weaver.Expectations]
+  ): Unit =
+    test(tn)((res, log) => f(log))
 
-//     val capabilities =
-//       ServerCapabilities(
-//         hoverProvider = Opt(true),
-//         documentSymbolProvider =
-//           Opt(DocumentSymbolOptions(label = Opt("howdy")))
-//       )
+  serverTest("initialize"): log =>
+    Probe
+      .create(
+        basicServer
+          .handleRequest(initialize): in =>
+            IO(
+              InitializeResult(
+                ServerCapabilities(
+                  hoverProvider = Opt(true),
+                  documentSymbolProvider =
+                    Opt(DocumentSymbolOptions(label = Opt("howdy")))
+                ),
+                serverInfo = Some(
+                  ServerInfo(name =
+                    in.params.clientInfo.get.name + " responder"
+                  )
+                )
+              )
+            ),
+        log
+      )
+      .use: probe =>
+        val expected =
+          InitializeResult(
+            ServerCapabilities(
+              hoverProvider = Some(true),
+              documentSymbolProvider =
+                Some(DocumentSymbolOptions(label = Some("howdy")))
+            ),
+            serverInfo = Some(ServerInfo("shrimp responder"))
+          )
 
-//     val server = basicServer[Try].handleRequest(initialize) { in =>
-//       Try {
-//         InitializeResult(capabilities)
-//       }
-//     }
+        for response <- probe.request(
+            CallId.StringId("resp1"),
+            initialize,
+            InitializeParams(
+              processId = Opt(25),
+              rootUri = Opt(DocumentUri("/howdy")),
+              capabilities = ClientCapabilities(),
+              clientInfo = Some(ClientInfo("shrimp"))
+            )
+          )
+        yield expect.same(expected, response)
+        end for
 
-//     val (response, _) = request(
-//       server,
-//       CallId.StringId("resp1"),
-//       initialize,
-//       InitializeParams(
-//         processId = Opt(25),
-//         rootUri = Opt(DocumentUri("/howdy")),
-//         capabilities = ClientCapabilities()
-//       )
-//     ).get
+  serverTest("didOpen") { log =>
+    import requests.*
 
-//     expect.same(response.capabilities, capabilities)
+    val server = basicServer.handleNotification(textDocument.didOpen) { in =>
+      log.info("in handler") *>
+        in.toClient.notification(
+          window.showMessage(
+            ShowMessageParams(
+              MessageType.Info,
+              s"you opened a ${in.params.textDocument.languageId} document from ${in.params.textDocument.uri}!"
+            )
+          )
+        ) *>
+        IO.sleep(100.millis) *>
+        in.toClient.notification(
+          window.showMessage(
+            ShowMessageParams(
+              MessageType.Info,
+              "And hello to yous"
+            )
+          )
+        )
+    }
 
-//   }
+    Probe
+      .create(server, log)
+      .use: probe =>
+        for
+          _ <- probe.notification(
+            textDocument.didOpen,
+            DidOpenTextDocumentParams(
+              TextDocumentItem(
+                uri = DocumentUri("/home/bla.txt"),
+                languageId = "text",
+                version = 0,
+                text = "Hello!"
+              )
+            )
+          )
+          nots <- probe.waitForNotifications(window.showMessage, 2)
+          (first, second) = (nots.head, nots.tail.head)
+        yield expect.same(
+          ShowMessageParams(
+            MessageType.Info,
+            s"you opened a text document from /home/bla.txt!"
+          ),
+          first
+        ) && expect.same(
+          ShowMessageParams(
+            MessageType.Info,
+            "And hello to yous"
+          ),
+          second
+        )
 
-//   test("didOpen") {
-//     import requests.*
+  }
 
-//     val server = basicServer[Try].handleNotification(textDocument.didOpen) {
-//       in =>
-//         in.toClient.notification(
-//           window.showMessage,
-//           ShowMessageParams(
-//             MessageType.Info,
-//             s"you opened a ${in.params.textDocument.languageId} document from ${in.params.textDocument.uri}!"
-//           )
-//         )
-//     }
+  serverTest("documentSymbol") { log =>
+    val symbols: Option[Vector[DocumentSymbol]] =
+      Option(
+        Vector(
+          DocumentSymbol(
+            name = "root",
+            kind = SymbolKind.Array,
+            range = Range(Position(0, 0), Position(0, 5)),
+            selectionRange = Range(Position(0, 0), Position(0, 5)),
+            children = Option(
+              Vector(
+                DocumentSymbol(
+                  name = "child",
+                  kind = SymbolKind.String,
+                  range = Range(Position(1, 0), Position(1, 5)),
+                  selectionRange = Range(Position(1, 0), Position(1, 5))
+                )
+              )
+            )
+          )
+        )
+      )
 
-//     val back = notification(
-//       server,
-//       textDocument.didOpen,
-//       DidOpenTextDocumentParams(
-//         TextDocumentItem(
-//           uri = DocumentUri("/home/bla.txt"),
-//           languageId = "text",
-//           version = 0,
-//           text = "Hello!"
-//         )
-//       )
-//     ).get
+    end symbols
 
-//     expect.same(
-//       back.collect(window.showMessage).get,
-//       List(
-//         ShowMessageParams(
-//           MessageType.Info,
-//           "you opened a text document from /home/bla.txt!"
-//         )
-//       )
-//     )
+    try textDocument.documentSymbol.outputToJson(symbols)
+    catch
+      case e: Throwable =>
+        e.printStackTrace()
 
-//   }
+    textDocument.documentSymbol.outputToJson(symbols)
+
+    val server = basicServer.handleRequest(textDocument.documentSymbol) { in =>
+      IO(symbols)
+    }
+
+    Probe.create(server, log).use { probe =>
+      for response <- probe.request(
+          CallId.StringId("symbols"),
+          textDocument.documentSymbol,
+          DocumentSymbolParams(
+            TextDocumentIdentifier(DocumentUri("/home/bla.txt"))
+          )
+        )
+      yield expect.same(symbols, response)
+    }
+
+  }
+end LSPTests
+
+def basicServer =
+  LSPBuilder.create[IO]
 
 //   test("textDocument/documentSymbol") {
 //     import requests.*
 
-//     val symbols: Opt[Vector[DocumentSymbol]] =
+//     val symbols: Option[Vector[DocumentSymbol]] =
 //       Opt(
 //         Vector(
 //           DocumentSymbol(
---
+++ modules/tests/src/test/scala/Probe.scala
@@ -0,0 +1,179 @@
+/*
+ * Copyright 2022 Neandertech
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tests.core
+
+import scala.concurrent.duration.*
+
+import _root_.fs2.*
+import _root_.fs2.concurrent.SignallingRef
+import _root_.fs2.concurrent.Topic
+import cats.effect.Deferred
+import cats.effect.IO
+import cats.effect.Ref
+import cats.syntax.all.*
+import jsonrpclib.InputMessage.NotificationMessage
+import jsonrpclib.OutputMessage.ResponseMessage
+import jsonrpclib.fs2.FS2Channel
+import jsonrpclib.fs2.catsMonadic
+import jsonrpclib.{fs2 as _, *}
+import langoustine.lsp.*
+import langoustine.lsp.all.*
+
+
+class Probe(
+    messagesFromServer: SignallingRef[IO, Vector[Message]],
+    send: Message => IO[Unit],
+    pendingRequests: Ref[IO, Map[CallId, Deferred[IO, Message]]],
+    log: weaver.Log[IO]
+):
+
+  def notification[T <: LSPNotification](t: T, params: t.In): IO[Unit] =
+    val msg = InputMessage.NotificationMessage(
+      method = t.notificationMethod,
+      params = Some(Payload(t.inputToJson.apply(params)))
+    )
+
+    send(msg)
+
+  def waitForNotifications[T <: LSPNotification](
+      t: T,
+      count: Int
+  ): IO[Vector[t.In]] =
+
+    IO.ref(Vector.empty[t.In])
+      .flatMap { ref =>
+        def collect: PartialFunction[Message, IO[t.In]] =
+          case InputMessage.NotificationMessage(method, payload)
+              if method == t.notificationMethod =>
+            IO.fromEither(
+              t.inputFromJson.decodeJson(
+                payload.map(_.data).getOrElse(_root_.io.circe.Json.Null)
+              )
+            )
+
+        for
+          initial <- messagesFromServer.get
+
+          matching <- initial.traverseCollect(collect)
+
+          _ <- log.info(
+            s"Waiting for ${count} notifications with [${t.notificationMethod}]: collected ${matching.length} on startup"
+          )
+
+          sizes = _root_.fs2.Stream
+            .repeatEval(ref.get.map(_.length))
+            .metered(50.millis)
+
+          _ <- messagesFromServer.discrete
+            .evalMap { vec =>
+              vec.traverseCollect(collect).flatMap { selected =>
+                ref.set(selected).as(selected.length)
+              }
+            }
+            .takeWhile(_ != count)
+            .compile
+            .drain
+
+          all <- ref.get
+        yield all
+        end for
+      }
+      .timeout(2.second)
+  end waitForNotifications
+
+  def request[T <: LSPRequest](callId: CallId, t: T, params: t.In): IO[t.Out] =
+    val msg = InputMessage.RequestMessage(
+      method = t.requestMethod,
+      callId = callId,
+      params = Some(Payload(t.inputToJson.apply(params)))
+    )
+
+    send(msg) *> pendingRequests.get.flatMap(
+      _.get(callId)
+        .map(_.get.timeout(2.seconds).flatMap { msg =>
+          msg match
+            case ResponseMessage(callId, data) =>
+              IO.fromEither(t.outputFromJson.decodeJson(data.data))
+            case other =>
+              IO.raiseError(
+                RuntimeException(
+                  s"unknown response type to call id $callId: $other"
+                )
+              )
+        })
+        .getOrElse(
+          IO.raiseError(
+            RuntimeException(s"no pending request for call id $callId")
+          )
+        )
+    )
+  end request
+end Probe
+
+object Probe:
+  def create(builder: LSPBuilder[IO], log: weaver.Log[IO]) =
+    for
+      ch <- FS2Channel
+        .resource[IO]()
+        .flatMap(ch => ch.withEndpoints(builder.build(Communicate.channel(ch))))
+      input <- Topic[IO, Message]().toResource
+      _     <- input
+        .subscribe(1)
+        .evalTap(msg => log.info(s"Sending message $msg"))
+        .through(ch.input)
+        .compile
+        .drain
+        .background
+      messagesFromServer <- SignallingRef[IO, Vector[Message]](
+        Vector.empty[Message]
+      ).toResource
+      pendingRequests <- IO
+        .ref(Map.empty[CallId, Deferred[IO, Message]])
+        .toResource
+      _ <- ch.output
+        .evalTap(msg => log.info(s"Outputting message $msg"))
+        .through(_.evalMap { msg =>
+          msg.maybeCallId.traverse { cid =>
+            pendingRequests.get.map(_.get(cid)).flatMap {
+              case None =>
+                IO.raiseError(
+                  RuntimeException(s"no pending request for call id $cid")
+                )
+              case Some(dfr) => dfr.complete(msg)
+            }
+          } *> messagesFromServer.update(_ :+ msg)
+        })
+        .compile
+        .drain
+        .background
+      send = (msg: Message) =>
+        msg.maybeCallId
+          .map(cid =>
+            IO.deferred[Message]
+              .flatMap(dfr => pendingRequests.update(_ + (cid -> dfr)))
+          )
+          .getOrElse(IO.unit) *>
+          input
+            .publish1(msg)
+            .flatMap(
+              _.fold(
+                _ => IO.raiseError(RuntimeException("channel already closed")),
+                _ => IO.unit
+              )
+            )
+    yield Probe(messagesFromServer, send, pendingRequests, log)
+end Probe
